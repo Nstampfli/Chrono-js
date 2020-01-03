67e1da285908294804a2ce62f5abbd1d952006f7
Nstampfli <nathan.stampfli@hetic.net>
2020-01-03 22:04:24 +0100

    import all code without reset

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let sec = 0;
+let min = 0;
+let hour = 0;
+let alertFuncTemplate;
+let time = document.querySelector(".time");
+let start = document.querySelector(".start");
+let stop = document.querySelector(".stop");
+let reset = document.querySelector(".reset");
+let on = false;
+
+function myFunction() {
+  alertFuncTemplate = setInterval(alertFunc, 1000);
+}
+
+function alertFunc() {
+  sec++;
+  if (sec === 59) {
+    min++;
+    sec = 0;
+  }
+  if (min === 59) {
+    hour++;
+    min = 0;
+  }
+  //Check Sec
+  if (sec < 10 && min < 10 && hour < 10) {
+    time.textContent = "0" + hour + ":0" + min + ":0" + sec;
+  }
+  if (sec >= 10 && min < 10 && hour < 10) {
+    time.textContent = "0" + hour + ":0" + min + ":" + sec;
+  }
+  if (sec >= 10 && min >= 10 && hour < 10) {
+    time.textContent = "0" + hour + ":" + min + ":" + sec;
+  }
+  if (sec >= 10 && min >= 10 && hour >= 10) {
+    time.textContent = "" + hour + ":" + min + ":" + sec;
+  }
+  //Check Min
+  if (sec < 10 && min >= 10 && hour < 10) {
+    time.textContent = "0" + hour + ":" + min + ":0" + sec;
+  }
+  if (sec < 10 && min >= 10 && hour >= 10) {
+    time.textContent = "" + hour + ":0" + min + ":0" + sec;
+  }
+  //Check Hour
+  if (sec < 10 && min < 10 && hour >= 10) {
+    time.textContent = hour + ":0" + min + ":0" + sec;
+  }
+}
+
+start.addEventListener("click", function() {
+  if (on === false) {
+    myFunction();
+    on = true;
+  }
+});
+
+stop.addEventListener("click", function() {
+  clearTimeout(alertFuncTemplate);
+  on = false;
+});
+
+reset.addEventListener("click", function() {
+  let sec = 0;
+  let min = 0;
+  let hour = 0;
+  time.textContent = "0" + hour + ":0" + min + ":0" + sec;
+});
